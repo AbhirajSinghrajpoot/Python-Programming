50c5b7355895c12993f6e1940f31cd98a8f6f943
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-04-12 09:49:25 +0530

    Add Day 1 exercises: Hello World, Brand Name Generator, Input handling, and String Manipulation

---
+++ Day 1/BrandNameGenerator.py
@@ -0,0 +1,10 @@
+# Create a greeting for your program
+# Ask the user for the city that they grew up in
+# Ask the user for the name of a pet
+# Combine their city and pet name to show their band name
+# Make sure the input cursor shows on a new line
+
+print("Welcome to the Brand Name Generator!")
+city = input("what's name of the city you grew up in? : \n")
+pet = input("what's your pet's name? : \n")
+print("your brand name could be " + city + " " + pet)
