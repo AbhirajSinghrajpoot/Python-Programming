2e574e584479300018986115ffbabb86d8f65c5b
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-05-21 10:07:25 +0530

    Add Day 14 - Update game logo and add C/C++ configuration files

--- Day_14/game_data.py
+++ Day_14/game_data.py
@@ -5,22 +5,298 @@ data = [
         'description': 'social media platform',
         'country': 'United States'
     },
-        {
+    {
         'name': 'Cristiano Ronaldo',
-        'follower_count': 215,
+        'follower_count': 640,
         'description': 'Footballer',
         'country': 'Portugal'
     },
-        {
+    {
         'name': 'Ariana Grande',
-        'follower_count': 183,
-        'description': 'Musician and actress',
+        'follower_count': 380,
+        'description': 'Musician and actor',
         'country': 'United States'
     },
-        {
+    {
         'name': 'Dwayne Johnson',
-        'follower_count': 181,
+        'follower_count': 395,
         'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 430,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 400,
+        'description': 'Reality TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 365,
+        'description': 'Reality TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 510,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Beyonce',
+        'follower_count': 320,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 295,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 285,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 225,
+        'description': 'Footballer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 290,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 255,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 225,
+        'description': 'Rapper',
+        'country': 'United States'
+    },
+    {
+        'name': 'Khloe Kardashian',
+        'follower_count': 305,
+        'description': 'Reality TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Miley Cyrus',
+        'follower_count': 215,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 205,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 180,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 190,
+        'description': 'Actor and singer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Rihanna',
+        'follower_count': 155,
+        'description': 'Musician and entrepreneur',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'Chris Brown',
+        'follower_count': 145,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 145,
+        'description': 'Rapper',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 115,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dua Lipa',
+        'follower_count': 115,
+        'description': 'Musician',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Cardi B',
+        'follower_count': 170,
+        'description': 'Rapper',
+        'country': 'United States'
+    },
+    {
+        'name': 'Emma Watson',
+        'follower_count': 75,
+        'description': 'Actor',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Robert Downey Jr',
+        'follower_count': 70,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Chris Hemsworth',
+        'follower_count': 60,
+        'description': 'Actor',
+        'country': 'Australia'
+    },
+    {
+        'name': 'Tom Holland',
+        'follower_count': 70,
+        'description': 'Actor',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Leonardo DiCaprio',
+        'follower_count': 65,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Will Smith',
+        'follower_count': 70,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 90,
+        'description': 'Actor and producer',
+        'country': 'India'
+    },
+    {
+        'name': 'Deepika Padukone',
+        'follower_count': 80,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Shah Rukh Khan',
+        'follower_count': 47,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Salman Khan',
+        'follower_count': 65,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Akshay Kumar',
+        'follower_count': 70,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Alia Bhatt',
+        'follower_count': 85,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 260,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'MS Dhoni',
+        'follower_count': 50,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'David Beckham',
+        'follower_count': 85,
+        'description': 'Footballer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 90,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 70,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Gigi Hadid',
+        'follower_count': 80,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Bella Hadid',
+        'follower_count': 60,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Hailey Bieber',
+        'follower_count': 55,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ryan Reynolds',
+        'follower_count': 60,
+        'description': 'Actor',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Vin Diesel',
+        'follower_count': 85,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Johnny Depp',
+        'follower_count': 60,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Scarlett Johansson',
+        'follower_count': 70,
+        'description': 'Actor',
         'country': 'United States'
     }
 ]
