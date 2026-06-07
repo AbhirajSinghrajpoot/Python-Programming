0bf5f3bed353cba9e0f6157f991794a488ebe21e
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-06-07 11:29:24 +0530

    Add weather notification functionality using Twilio and OpenWeatherMap API

---
+++ Day_35/Api key to authentication/main.py
@@ -0,0 +1,27 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "a421ad62eea9dae9d3d0db15c5a48016"
+
+weather_params = { 
+    "lat": 51.5074,
+    "lon": -0.1278, 
+    "appid": api_key 
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+weather_slice = weather_data["list"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+        
+if will_rain:
+    print("Bring an umbrella.")
+
---
+++ Day_35/Send SMS of Weather/main.py
@@ -0,0 +1,39 @@
+from twilio.rest import Client
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.getenv("OWM_API_KEY")
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+
+weather_params = { 
+    "q": "Mumbai",
+    "appid": api_key 
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+weather_slice = weather_data["list"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+        
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain.Remember to bring an umbrella.",
+        from_='+17154622315',
+        to='+91 62634 79576'
+    )
+
+print(message.status)
